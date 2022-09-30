3354718d54c815057f941cceb60f801112f9482d
darayavaus <darayavaus.chern@yandex.ru>
2022-09-30 19:17:03 +0300

    paper submission version

--- scripts/factorize.py
+++ scripts/factorize.py
@@ -13,130 +13,191 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import time
+import os
+import wandb
 
 from utils_torch import *
 
 from argparse import ArgumentParser
 
-
 parser = ArgumentParser()
+parser.add_argument("-m", "--method", dest="method", required=True, type=str,
+                    help="admm or parafac")
 parser.add_argument("-l", "--layer", dest="layer", required=True, type=str,
                     help="name of a layer to decompose(for example, layer2.1.conv1 means model.layer2[1].conv1)")
 parser.add_argument("-r", "--rank", dest="rank", required=True, type=int,
                     help="rank of decomposition")
-parser.add_argument("-m", "--method", dest="method", required=True, type=str,
-                    help="admm or parafac")
-parser.add_argument("-d", "--device", dest="device", required=True, type=str)
+parser.add_argument("-b", "--bits", dest="bits", required=True, type=int)
+parser.add_argument("-s", "--seed", dest="seed", required=True, type=int)
 
 args = parser.parse_args()
 
-if args.device == 'gpu':
-    device = torch.device('cuda:0')
-elif args.device == 'cpu':
-    device = 'cpu'
-else:
-    raise ValueError('Device can be one of [gpu, cpu]')
+bits = args.bits
+print('bits:', bits)
+
+# if args.device == 'gpu':
+#     device = torch.device('cuda:0')
+# elif args.device == 'cpu':
+#     device = 'cpu'
+# else:
+#     raise ValueError('Device can be one of [gpu, cpu]')
+device = torch.device('cuda:0')
 print('device:', device)
     
 orig_model = resnet18(pretrained=True)
 orig_model.eval()
 
-lname, lidx, ltype = args.layer.split('.')
-lidx = int(lidx)
-layer = orig_model.__getattr__(lname)[lidx].__getattr__(ltype)
-print('layer:', layer)
-
-kernel_size = layer.kernel_size
-stride = layer.stride
-padding = layer.padding
-cin = layer.in_channels
-cout = layer.out_channels
+if len(args.layer.split('.')) == 1:
+    layer = orig_model.__getattr__(args.layer)
+elif len(args.layer.split('.')) == 3:
+    lname, lidx, ltype = args.layer.split('.')
+    lidx = int(lidx)
+    layer = orig_model.__getattr__(lname)[lidx].__getattr__(ltype)
+    if ltype == 'downsample': layer = layer[0]
+else:
+    raise ValueError('Invalid layer name')
+print('layer:', args.layer)
 
 X = layer.weight.detach().to(device)
-X = X.reshape((X.shape[0], X.shape[1], -1))
 bias = layer.bias
 if bias is not None: bias = bias.detach().to(device)
-    
-scale = (X.max() - X.min()) / scale_denom
-zero_point = (-q - (X.min() / scale).int()).int()
-zero_point = torch.clamp(zero_point, -q, q - 1)
+if isinstance(layer, nn.Conv2d):
+    # kernel_size==1 => equivalent to a Linear layer 
+    if len(args.layer.split('.')) == 3 and ltype == 'downsample': 
+        X = X.reshape((X.shape[0], X.shape[1]))
+    else:
+        X = X.reshape((X.shape[0], X.shape[1], -1))
 
 init_method = 'random'
 rank = args.rank
 print('rank:', rank)
 
+seed = args.seed
+print('seed:', seed)
+
+method = args.method
+print('method:', method)
+
+outdir = f'{bits}bit_symmetric/factors_{method}_seed{seed}/'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
 start = time.time()
 
-print('method:', args.method)
-if args.method == 'admm':
-    A, B, C = init_factors(X, rank=rank, init='random', device=device, seed=0)
-    U_A = torch.zeros_like(A, device=device)
-    U_B = torch.zeros_like(B, device=device)
-    U_C = torch.zeros_like(C, device=device)
-    eps = 1e-10
-    max_iter = 1000
-#     alpha = 0.9
+if method == 'admm':
+    run = wandb.init(config=args, 
+                     project=f"admm-factorization-{bits}bit-symmetric", 
+                     entity="darayavaus",
+                     name=f'{method}_{args.layer}_rank{rank}_seed{seed}')
+    eps = 1e-8
+    tol = 1e-5
+    max_iter = 300
+    max_iter_factor = 1000
     loss_hist = []
     loss_quant_hist = []
+    wandb.config.update({"tol": tol, 
+                         "eps": eps,
+                         "max_iter": max_iter,
+                         "max_iter_factor": max_iter_factor})
+    # 3-tensor case
+    if X.ndim == 3:
+        A, B, C = init_factors(X, rank=rank, init=init_method, device=device, seed=seed)
+        U_A = torch.zeros_like(A, device=device)
+        U_B = torch.zeros_like(B, device=device)
+        U_C = torch.zeros_like(C, device=device)
 
-    for i in tqdm(range(300)):
-        G = B.T @ B * (C.T @ C)
-        # mttkrp
-        F = torch.einsum('abc,cr,br->ar', X, C, B)
-#         A, U_A = admm_iteration(A, U_A, F, G, max_iter=1000, alpha=alpha, eps=eps)
-        A, U_A = admm_iteration(A, U_A, F, G, max_iter=1000, eps=eps)
+        for i in tqdm(range(max_iter)):
+            G = B.T @ B * (C.T @ C)
+            # mttkrp
+            F = torch.einsum('abc,cr,br->ar', X, C, B)
+            A, U_A = admm_iteration(A, U_A, F, G, max_iter=max_iter_factor, eps=eps, bits=bits)
+            A_quantized = quantize_tensor(A, qscheme=torch.per_tensor_symmetric, bits=bits)
 
-        G = A.T @ A * (C.T @ C)
-        F = torch.einsum('abc,cr,ar->br', X, C, A)
-#         B, U_B = admm_iteration(B, U_B, F, G, max_iter=1000, alpha=alpha, eps=eps)
-        B, U_B = admm_iteration(B, U_B, F, G, max_iter=1000, eps=eps)
+            G = A.T @ A * (C.T @ C)
+            F = torch.einsum('abc,cr,ar->br', X, C, A)
+            B, U_B = admm_iteration(B, U_B, F, G, max_iter=max_iter_factor, eps=eps, bits=bits)
+            B_quantized = quantize_tensor(B, qscheme=torch.per_tensor_symmetric, bits=bits)
 
-        G = A.T @ A * (B.T @ B)
-        F = torch.einsum('abc,br,ar->cr', X, B, A)
-#         C, U_C = admm_iteration(C, U_C, F, G, max_iter=1000, alpha=alpha, eps=eps)
-        C, U_C = admm_iteration(C, U_C, F, G, max_iter=1000, eps=eps)
+            G = A.T @ A * (B.T @ B)
+            F = torch.einsum('abc,br,ar->cr', X, B, A)
+            C, U_C = admm_iteration(C, U_C, F, G, max_iter=max_iter_factor, eps=eps, bits=bits)
+            C_quantized = quantize_tensor(C, qscheme=torch.per_tensor_symmetric, bits=bits)
 
-        A_quantized = quantize_tensor(A, scale=scale, zero_point=zero_point)
-        B_quantized = quantize_tensor(B, scale=scale, zero_point=zero_point)
-        C_quantized = quantize_tensor(C, scale=scale, zero_point=zero_point)
-        loss_quant_hist.append(squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A_quantized, B_quantized, C_quantized)))
-        loss_hist.append(squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A, B, C)))
-        if len(loss_hist) > 1 and abs(loss_hist[-2] - loss_hist[-1]) < eps: 
-            break
-            
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
-    ax1.plot(loss_hist, '.')
-    ax1.set_title(f'Loss: {loss_hist[-1]:.5f}, Iterations: {len(loss_hist)}')
-    ax2.plot(loss_quant_hist, '.')
-    ax2.set_title(f'Loss: {loss_quant_hist[-1]:.5f}, Iterations: {len(loss_quant_hist)}')
-    plt.savefig('loss_admm.png')
-    
-    for mode, factor in enumerate([A,B,C]):
-        torch.save(factor, f'factors_proj/{args.layer}_admm_random_rank_{rank}_mode_{mode}.pt')
+            error = squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A, B, C))
+            quantized_error = squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A_quantized, B_quantized, C_quantized))
+            loss_hist.append(error)
+            loss_quant_hist.append(quantized_error)
+            wandb.log({'Reconstruction Error': error, 
+                       'Quantized Reconstruction Error': quantized_error})
+            if len(loss_hist) > 1 and abs(loss_hist[-2] - loss_hist[-1]) < tol: 
+                break 
+            # safe against exploding error
+            if len(loss_hist) > 10 and loss_hist[-1] - loss_hist[-10] > 1e-3:
+                break
 
-elif args.method == 'parafac':
+        factors = [A, B, C]
+        factors_quantized = [A_quantized, B_quantized, C_quantized]
+    # matrix case
+    elif X.ndim == 2:
+        A, B = init_factors(X, rank=rank, init=init_method, device=device, seed=seed)
+        U_A = torch.zeros_like(A, device=device)
+        U_B = torch.zeros_like(B, device=device)
+        
+        for i in tqdm(range(max_iter)):
+            G = B.T @ B
+            F = X @ B
+            A, U_A = admm_iteration(A, U_A, F, G, max_iter=max_iter_factor, eps=eps, bits=bits)
+            A_quantized = quantize_tensor(A, qscheme=torch.per_tensor_symmetric, bits=bits)
+
+            G = A.T @ A
+            F = X.T @ A
+            B, U_B = admm_iteration(B, U_B, F, G, max_iter=max_iter_factor, eps=eps, bits=bits)
+            B_quantized = quantize_tensor(B, qscheme=torch.per_tensor_symmetric, bits=bits)
+
+            error = squared_relative_diff(X, A@B.T)
+            quantized_error = squared_relative_diff(X, A_quantized@B_quantized.T)
+            loss_hist.append(error)
+            loss_quant_hist.append(quantized_error)
+            wandb.log({'Reconstruction Error': error, 
+                       'Quantized Reconstruction Error': quantized_error})
+            if len(loss_hist) > 1 and abs(loss_hist[-2] - loss_hist[-1]) < tol: 
+                break 
+            # safe against exploding error
+            if len(loss_hist) > 10 and loss_hist[-1] - loss_hist[-10] > 1e-3:
+                break
+                
+            factors = [A, B]
+            factors_quantized = [A_quantized, B_quantized]
+        
+    else: 
+        raise ValueError('Incorrect number of dimentions in X')
+
+    torch.save(loss_hist, f'{outdir}/{args.layer}_{method}_{init_method}_rank_{rank}_losshist.pt')
+    torch.save(loss_quant_hist, f'{outdir}/{args.layer}_{method}_{init_method}_rank_{rank}_lossquanthist.pt')
+
+elif method == 'parafac':
     tl.set_backend('pytorch')
-    # _, factors = parafac(X, rank=rank, init=init_method, tol=1e-10, n_iter_max=10000, random_state=0)
-    # _, factors = parafac(X, rank=rank, init=init_method, stop_criterion='rec_error_deviation', tol=1e-10, n_iter_max=10000,  random_state=0)
-    _, factors = parafac(X, rank=rank, init=init_method, random_state=0)
-    A,B,C = factors
-    A_quantized = quantize_tensor(A)
-    B_quantized = quantize_tensor(B)
-    C_quantized = quantize_tensor(C)
+    _, factors = parafac(X, rank=rank, init=init_method, random_state=seed, 
+                         tol=1e-8, stop_criterion='rec_error_deviation', n_iter_max=5000)
+    factors_quantized = [quantize_tensor(factor, qscheme=torch.per_tensor_symmetric, bits=bits) for factor in factors]
     
-#     for mode, factor in enumerate(factors):
-#         torch.save(factor, f'../factors/{layer_name}_parafac_{init_method}_errdev_rank_{rank}_mode_{mode}.pt')
-    
-    for mode, factor in enumerate(factors):
-        torch.save(factor, f'../factors/{args.layer}_parafac_{init_method}_errdev_default_rank_{rank}_mode_{mode}.pt')
-
 else:
-    raise NotImplementedError('Invalid method:', args.method)
-
-print(squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A, B, C)),
-      squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', A_quantized, B_quantized, C_quantized)))
+    raise ValueError('Method can be one of [admm, parafac]')
 
 end = time.time()
-print('Factorization took {} minutes'.format((end-start)/60))
 
+for mode, factor in enumerate(factors):
+    torch.save(factor, f'{outdir}/{args.layer}_{method}_{init_method}_rank_{rank}_mode_{mode}.pt')
 
+if X.ndim == 3:
+    print('Factorization took {} minutes'.format((end-start)/60))
+    print('Factorization error is {} for usual and {} for quantized'.format(
+        squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', *factors)),
+        squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', *factors_quantized))
+    ))
+elif X.ndim == 2:
+    pass
+else:
+    raise ValueError('Incorrect number of dimentions in X')
+
+if method == 'admm': run.finish()
--- scripts/utils_torch.py
+++ scripts/utils_torch.py
@@ -1,5 +1,9 @@
 import numpy as np
 import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+from collections import OrderedDict
 
 
 def squared_relative_diff(X, Y):
@@ -19,26 +23,59 @@ def init_factors(tensor, rank, init='svd', device=None, seed=None):
     if init == 'random':
         for mode in range(tensor.ndim):
             factors.append(torch.randn(tensor.shape[mode], rank, generator=gen, device=device))
+    elif init == 'svd':
+        print('init with svd')
+        for mode in range(tensor.ndim):
+            U, _, _ = torch.linalg.svd(unfold(tensor, mode), full_matrices=False)
+            if tensor.shape[mode] < rank: 
+                random_part = torch.randn(U.shape[0], rank - tensor.shape[mode], generator=gen, device=device)
+                U = torch.cat((U, random_part), axis=1)
+            factors.append(U[:, :rank])
     else:
         raise NotImplementedError(init)
         
     return factors
 
 
-def quantize_tensor_wrapper(q, scale_denom, dtype):
-    def _wrapper(x, scale=None, zero_point=None):
-        if scale is None or zero_point is None:
-            scale = (x.max() - x.min()) / scale_denom 
-            zero_point = (-q - (x.min() / scale).int()).int()
-            zero_point = torch.clamp(zero_point, -q, q - 1)
+def quantize_tensor_wrapper(qscheme, bits):
+    def _wrapper(x, xmin=None, xmax=None):    
+        q = 2 ** (bits-1)
+        scale_denom = 2*q - 1
         
-        return torch.quantize_per_tensor(x, scale=scale, zero_point=zero_point, dtype = dtype).dequantize()
+        if xmin is None: xmin = x.min()
+        if xmax is None: xmax = x.max()
+        
+        if qscheme == torch.per_tensor_symmetric:
+            scale = 2 * torch.where(xmin.abs() > xmax, xmin.abs(), xmax) / scale_denom
+            zero_point = torch.zeros(scale.shape).int()
+                
+            return torch.clamp(torch.round(x / scale), -q, q-1).to(int) * scale
+        
+        elif qscheme == torch.per_tensor_affine:
+            scale = (xmax - xmin) / scale_denom 
+            zero_point = (-q - (xmin / scale).int()).int()
+            zero_point = torch.clamp(zero_point, -q, q - 1)
+            
+            return (torch.clamp(torch.round(x / scale) + zero_point, -q, q-1).to(int) - zero_point) * scale
+    
     return _wrapper
 
 
-q = torch.tensor(2**7).float()
-scale_denom = torch.tensor(2**8 - 1).float()
-quantize_tensor = quantize_tensor_wrapper(q, scale_denom, dtype=torch.qint8)
+def quantize_tensor(x, qscheme, bits, scale=None):
+    q = 2 ** (bits-1)
+    scale_denom = 2*q - 1
+        
+    xmin = x.min()
+    xmax = x.max()
+        
+    if qscheme == torch.per_tensor_symmetric:
+        if scale is None:
+            scale = 2 * torch.where(xmin.abs() > xmax, xmin.abs(), xmax) / scale_denom
+
+        return torch.clamp(torch.round(x / scale), -q, q-1).to(int) * scale
+
+    else:
+        raise NotImplementedError(qscheme)
 
 
 def admm_iteration_old(H, U, F, G, max_iter, alpha, eps):
@@ -68,16 +105,15 @@ def admm_iteration_old(H, U, F, G, max_iter, alpha, eps):
     return H, U
 
 
-def admm_iteration(H, U, F, G, max_iter, eps):
+def admm_iteration(H, U, F, G, max_iter, eps, bits):
     rank = H.shape[1]
     rho = torch.trace(G) / rank
     L = torch.linalg.cholesky(G + rho * torch.eye(G.shape[0], device=G.device), upper=False)
     for j in range(1, max_iter):
-#         H_ = torch.linalg.inv(L.T) @ torch.linalg.inv(L) @ (F + rho * (H + U)).T
         H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
         H_T = H_.T
         H_prev = H.clone()
-        H = quantize_tensor(H_T - U)
+        H = quantize_tensor(H_T - U, qscheme=torch.per_tensor_symmetric, bits=bits, scale=None)
         U += H - H_T
 
         r = torch.sum((H - H_T)**2) / torch.sum(H**2)
@@ -86,3 +122,205 @@ def admm_iteration(H, U, F, G, max_iter, eps):
             break
             
     return H, U
+
+
+# no improvement over standart method
+def admm_iteration_polish(H, U, F, G, max_iter, eps):
+    rank = H.shape[1]
+    rho = torch.trace(G) / rank
+    L = torch.linalg.cholesky(G + rho * torch.eye(G.shape[0], device=G.device), upper=False)
+    for j in range(1, max_iter):
+        H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
+        H_prev = H.clone()
+        H = quantize_tensor(H_.T - U)
+        
+        # polishing
+        H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
+        
+        U += H - H_.T
+
+        r = torch.sum((H - H_.T)**2) / torch.sum(H**2)
+        s = torch.sum((H - H_prev)**2) / torch.sum(U**2) 
+        if r < eps and s < eps: 
+            break
+            
+    return H, U
+
+# takes too long
+def admm_iteration_neighs(H, U, F, G, X, neighs, factors, mode, max_iter, eps):
+    rank = H.shape[1]
+    device = H.device
+    rho = torch.trace(G) / rank
+    L = torch.linalg.cholesky(G + rho * torch.eye(G.shape[0], device=device), upper=False)
+    for j in range(1, max_iter):
+        H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
+
+        H_prev = H.clone()
+        H = quantize_tensor(H_.T - U)
+        
+        # polishing
+        H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
+
+        # neighbor search
+        # bestH = H.clone()
+        # bestH_ = H_.clone()
+        print('err 1', squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', *factors)))
+        factors[mode] = H_.T
+        for factor in factors:
+            print(factor.shape)
+        best_err = squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', *factors))
+        print('err 2', best_err)
+
+        scale = (H.max() - H.min()) / scale_denom 
+        zero_point = (-q - (H.min() / scale).int()).int()
+        zero_point = torch.clamp(zero_point, -q, q - 1)
+
+        ks = torch.randint(low=0, high=H.shape[0], size=(neighs,), device=device)
+        js = torch.randint(low=0, high=H.shape[1], size=(neighs,), device=device)
+        signs = torch.rand(size=(neighs,)) >= 0.5
+        print('looking for neighbors')
+        for k, j, sign in zip(ks, js, signs):
+            print(k,j,sign)
+            if sign: H[k,j] += scale
+            else: H[k,j] -= scale
+            H = quantize_tensor(H, scale=scale, zero_point=zero_point)
+            prev_H_ = H_.clone()
+            H_ = torch.cholesky_solve((F + rho * (H + U)).T, L, upper=False)
+            factors[mode] = H_.T
+            # error of convex objective (CP)
+            err = squared_relative_diff(X, torch.einsum('ir,jr,kr->ijk', *factors))
+            if err < best_err: 
+                print('Found a better neighbor with err', err)
+                best_err = err
+            else:
+                if sign: H[k,j] -= scale
+                else: H[k,j] += scale
+                H_ = prev_H_
+        
+        U += H - H_.T
+
+        r = torch.sum((H - H_.T)**2) / torch.sum(H**2)
+        s = torch.sum((H - H_prev)**2) / torch.sum(U**2) 
+        if r < eps and s < eps: 
+            break
+            
+    return H, U
+
+
+def accuracy(model, dataset_loader, device='cuda', num_classes=1000):
+    def one_hot(x, K):
+        return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
+    
+    # Set BN and Droupout to eval regime
+    model.eval()
+
+    total_correct = 0
+
+    for (x, y) in tqdm(dataset_loader):
+        x = x.to(device)
+        y = one_hot(np.array(y.numpy()), num_classes)
+        target_class = np.argmax(y, axis=1)
+
+        with torch.no_grad():
+            out = model(x).cpu().detach().numpy()
+            predicted_class = np.argmax(out, axis=1)
+            total_correct += np.sum(predicted_class == target_class)
+
+    total = len(dataset_loader) * dataset_loader.batch_size
+    return total_correct / total
+
+
+def calibrate(model, dataset_loader, device='cuda', num_batches=1000):
+    model.eval()
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Set BN to train regime
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.train()
+
+    batch_idx = 0
+    for (x, y) in tqdm(dataset_loader):
+        if batch_idx >= num_batches:
+            break
+
+        x = x.to(device)
+        with torch.no_grad():
+            _ = model(x)
+            
+        batch_idx += 1
+
+    return model
+
+
+def build_cp_layer(rank, factors, bias, cin, cout, kernel_size, padding, stride):
+    seq = nn.Sequential(OrderedDict([
+      ('conv1', nn.Conv2d(in_channels=cin, out_channels=rank, kernel_size=(1, 1), bias=False)),
+      ('conv2', nn.Conv2d(in_channels=rank, out_channels=rank, kernel_size=kernel_size,
+                          groups=rank, padding=padding, stride=stride, bias=False)),
+      ('conv3', nn.Conv2d(in_channels=rank, out_channels=cout, kernel_size=(1, 1), 
+                          bias=True if bias is not None else False)),
+    ]))
+
+    A,B,C = factors
+    f_cout = torch.unsqueeze(torch.unsqueeze(A, 2), 3)
+    f_cin = torch.unsqueeze(torch.unsqueeze(B.T, 2), 3)
+    f_z = torch.unsqueeze(torch.einsum('hwr->rhw', torch.reshape(C, (*kernel_size, rank))), 1)
+    assert seq.conv1.weight.data.shape == f_cin.shape, f'Expected shape: {seq.conv1.weight.data.shape}, but got {f_cin.shape}'
+    assert seq.conv2.weight.data.shape == f_z.shape, f'Expected shape: {seq.conv2.weight.data.shape}, but got {f_z.shape}'
+    assert seq.conv3.weight.data.shape == f_cout.shape, f'Expected shape: {seq.conv3.weight.data.shape}, but got {f_cout.shape}'
+    if bias is not None:
+        assert seq.conv3.bias.data.shape == bias.shape, f'Expected shape: {seq.conv3.bias.data.shape}, but got {bias.shape}'
+    with torch.no_grad():
+        seq.conv1.weight = nn.Parameter(f_cin, requires_grad=True)
+        seq.conv2.weight = nn.Parameter(f_z, requires_grad=True)
+        seq.conv3.weight = nn.Parameter(f_cout, requires_grad=True)
+        if bias is not None:
+            seq.conv3.bias = nn.Parameter(bias, requires_grad=True)
+
+    return seq
+
+
+def build_cp2conv_layer(rank, factors, bias, cin, cout, padding, stride):
+    seq = nn.Sequential(OrderedDict([
+      ('conv1', nn.Conv2d(in_channels=cin, out_channels=rank, kernel_size=(1, 1), padding=padding, stride=stride, bias=False)),
+      ('conv2', nn.Conv2d(in_channels=rank, out_channels=cout, kernel_size=(1, 1), 
+                          bias=True if bias is not None else False)),
+    ]))
+
+    A,B = factors
+    f_cout = torch.unsqueeze(torch.unsqueeze(A, 2), 3)
+    f_cin = torch.unsqueeze(torch.unsqueeze(B.T, 2), 3)
+    assert seq.conv1.weight.data.shape == f_cin.shape, f'Expected shape: {seq.conv1.weight.data.shape}, but got {f_cin.shape}'
+    assert seq.conv2.weight.data.shape == f_cout.shape, f'Expected shape: {seq.conv2.weight.data.shape}, but got {f_cout.shape}'
+    if bias is not None:
+        assert seq.conv2.bias.data.shape == bias.shape, f'Expected shape: {seq.conv2.bias.data.shape}, but got {bias.shape}'
+    with torch.no_grad():
+        seq.conv1.weight = nn.Parameter(f_cin, requires_grad=True)
+        seq.conv2.weight = nn.Parameter(f_cout, requires_grad=True)
+        if bias is not None:
+            seq.conv2.bias = nn.Parameter(bias, requires_grad=True)
+
+    return seq
+
+
+def build_cpfc_layer(rank, factors, bias, fin, fout):
+    seq = nn.Sequential(OrderedDict([
+      ('fc1', nn.Linear(in_features=fin, out_features=rank, bias=False)),
+      ('fc2', nn.Linear(in_features=rank, out_features=fout, bias=True if bias is not None else False)),
+    ]))
+
+    # fc weight is transposed
+    B, A = factors
+    assert seq.fc1.weight.data.shape == A.T.shape, f'Expected shape: {seq.fc1.weight.data.shape}, but got {A.T.shape}'
+    assert seq.fc2.weight.data.shape == B.shape, f'Expected shape: {seq.fc2.weight.data.shape}, but got {B.shape}'
+    if bias is not None:
+        assert seq.fc2.bias.data.shape == bias.shape, f'Expected shape: {seq.fc2.bias.data.shape}, but got {bias.shape}'
+    with torch.no_grad():
+        seq.fc1.weight = nn.Parameter(A.T, requires_grad=True)
+        seq.fc2.weight = nn.Parameter(B, requires_grad=True)
+        if bias is not None:
+            seq.fc2.bias = nn.Parameter(bias, requires_grad=True)
+
+    return seq
